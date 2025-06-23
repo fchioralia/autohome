a51d7d841d23422137f2786dfcc46d3baf535018
Florin Chioralia <fchioralia@gmail.com>
2025-06-23 22:39:16 +0300

    fix: update Django documentation link and clean up URL patterns

--- auto_sprinklers/sprinklers/urls.py
+++ auto_sprinklers/sprinklers/urls.py
@@ -1,7 +1,7 @@
 """home URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import views
 
-#app_name = 'sprinklers'
+# app_name = 'sprinklers'
 urlpatterns = [
     path('', views.index, name='index'),
-#    path('sprinkler/<int:sprinkler_gpio>/', views.get_sprinkler_enable, name='get_sprinkler_enable'),
+    # path('sprinkler/<int:sprinkler_gpio>/', views.get_sprinkler_enable, name='get_sprinkler_enable'),
     path('get_sprinkler_enabled/<int:sprinkler_gpio>/', views.get_sprinkler_enabled, name='get_sprinkler_enabled'),
     path('get_sprinkler_active/<int:sprinkler_gpio>/', views.get_sprinkler_active, name='get_sprinkler_active'),
     path('get_sensor_enabled/<int:sensor_gpio>/', views.get_sensor_enabled, name='get_sensor_enabled'),
@@ -33,5 +32,6 @@ urlpatterns = [
     path('set_sprinkler_active/<int:sprinkler_gpio>', views.set_sprinkler_active, name='set_sprinkler_active'),
     path('set_service_active/<slug:service>/<slug:status>', views.set_service_active, name='set_service_active'),
     path('set_service_enabled/<slug:service>/<slug:status>', views.set_service_enabled, name='set_service_enabled'),
-#    url('sensor/run/',views.run_sprinklers_service,name='run_sprinklers_service'),
+    # Example of regex route with re_path (uncomment and adapt if needed):
+    # re_path(r'^sensor/run/$', views.run_sprinklers_service, name='run_sprinklers_service'),
 ]
